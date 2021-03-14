c15f856014f7a13705ef5cb310ca72bd999c74bd
mtfffg <mario.younis@yahoo.com>
2021-03-13 20:06:49 -0500

    Add files via upload

---
+++ infraredproject/home.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+});
+
+$('.linkheader').mouseover(function () {
+    $('.linkscollection').slideDown();
+});
+
+$('.linkheader').mouseleave(function () {
+    $('.linkscollection').slideUp();
+});
