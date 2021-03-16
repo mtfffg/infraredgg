3b90b170930cdc957598bbadae43f25eadd64f01
mtfffg <mario.younis@yahoo.com>
2021-03-16 13:55:35 -0400

    Add files via upload

--- infraredproject/home.js
+++ infraredproject/home.js
@@ -2,28 +2,69 @@ $(document).ready(function () {
     //if (window.location.href.indexOf("www") < 0) {
     //    window.location.href = 'https://www.infrared.gg';
     //}
-
 });
 
 
-$(document).on('keydown', function (e) {
-    var keyCode = e.keyCode || e.which;
+$('.mainGlobe').click(function () {
+    if ($('.debateDiv').is(':visible')) {
+        $('.debateDiv').css('display', 'none');
+        $('#twitterDiv').css('width', '25%');
+        $('.viddiv').css('left', '25%').css('width', '75%');
+        $('.risingDiv').hide();
+        $('#fightsong')[0].pause()
+        $('#fightsong').hide();
+    }
+    else {
+        $('.debateDiv').css('display', 'inline-block');
+        $('#twitterDiv').css('width', '20%');
+        $('.viddiv').css('left', '40%').css('width', '60%');
+        $('.risingDiv').show();
+        $('#fightsong').show();
+        $('#fightsong')[0].play();
+    }
+});
 
-    if (keyCode == 27) {
-        if ($('.debateDiv').is(':visible')) {
-            $('#fightsong')[0].pause()
-            $('.debateDiv').css('display', 'none');
-            $('#twitterDiv').css('width', '25%');
-            $('.viddiv').css('left', '25%').css('width', '75%');
-            $('.risingDiv').hide();
-        }
-        else {
-            $('#fightsong')[0].play();
-            $('.debateDiv').css('display', 'inline-block');
-            $('#twitterDiv').css('width', '20%');
-            $('.viddiv').css('left', '40%').css('width', '60%');
-            $('.risingDiv').show();
-        }
+$('.debatebox').click(function () {
+    var id = $(this).data('id');
+
+    switch (id) {
+        case 4://vaush
+            window.open(
+                "https://www.youtube.com/watch?v=T-djYWAhrnM", "_blank");
+            break;
+        case 5://destiny
+            window.open(
+                "https://www.youtube.com/watch?v=HB55g2kunNU", "_blank");
+            break;
+        case 6://fellow traveler
+            window.open(
+                "https://www.youtube.com/watch?v=AM838wusAmQ", "_blank");
+            break;
+        case 7://jolah
+            window.open(
+                "https://www.youtube.com/watch?v=Mml7rXLQoTQ", "_blank");
+            break;
+        case 8://leftism today
+            window.open(
+                "https://www.youtube.com/watch?v=mDM2ojVYdrw", "_blank");
+            break;
+        case 9://socialism done left
+            window.open(
+                "https://www.youtube.com/watch?v=xIlceNoYvGs", "_blank");
+            break;
+        case 10://xanderthal
+            window.open(
+                "https://www.youtube.com/watch?v=FFsIPOuOBg4", "_blank");
+            break;
+        case 11://kevin logan
+            window.open(
+                "https://www.youtube.com/watch?v=CitM_kAZ_9M", "_blank");
+            break;
+        case 12://dylan burns tv and sondsol
+        case 13:
+            window.open(
+                "https://www.youtube.com/watch?v=7UpCH57lcbQ", "_blank");
+            break;
     }
 });
 
@@ -34,3 +75,9 @@ $(document).on('keydown', function (e) {
 //$('.linkheader').mouseleave(function () {
 //    $('.linkscollection').slideUp();
 //});
+
+
+
+//RewriteEngine on
+//RewriteCond % { HTTP_HOST } ^ infrared\.gg$
+//RewriteRule ^ /?$ "https\:\/\/www\.infrared\.gg\/" [R=301,L]
