2aeeca22c0d0094e7cebe0978218a796f4a87a4a
mtfffg <mario.younis@yahoo.com>
2021-03-16 15:49:37 -0400

    Add files via upload

--- infraredproject/home.js
+++ infraredproject/home.js
@@ -33,57 +33,57 @@ $('.debatebox').click(function () {
     switch (id) {
         case 4://vaush
             open = true;
-            modaltitle = 'INFRARED VS VAUSH';            
-            modalbody = '<div class="youtubevod" style="height:75vh;"><iframe width="100%" height="100%" src="https://invidious.snopyta.org/embed/T-djYWAhrnM" frameborder="0" allow="accelerometer;' +
+            modaltitle = 'INFRARED VS VAUSH';                        
+            modalbody = '<div class="youtubevod" style="height:75vh;"><iframe width="100%" height="100%" src="https://yewtu.be/embed/T-djYWAhrnM" frameborder="0" allow="accelerometer;' +
                 ' autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>';                   
             break;
         case 5://destiny
             open = true;
             modaltitle = 'INFRARED VS DESTINY';
-            modalbody = '<div class="youtubevod" style="height:75vh;"><iframe width="100%" height="100%" src="https://invidious.snopyta.org/embed/HB55g2kunNU" frameborder="0" allow="accelerometer;' +
+            modalbody = '<div class="youtubevod" style="height:75vh;"><iframe width="100%" height="100%" src="https://yewtu.be/embed/HB55g2kunNU" frameborder="0" allow="accelerometer;' +
                 ' autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>';
             break;
         case 6://fellow traveler
             open = true;
             modaltitle = 'INFRARED VS FELLOW TRAVELER';
-            modalbody = '<div class="youtubevod" style="height:75vh;"><iframe width="100%" height="100%" src="https://invidious.snopyta.org/embed/AM838wusAmQ" frameborder="0" allow="accelerometer;' +
+            modalbody = '<div class="youtubevod" style="height:75vh;"><iframe width="100%" height="100%" src="https://yewtu.be/embed/AM838wusAmQ" frameborder="0" allow="accelerometer;' +
                 ' autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>';
             break;
         case 7://jolah
             open = true;
             modaltitle = 'INFRARED VS JOLAH';
-            modalbody = '<div class="youtubevod" style="height:75vh;"><iframe width="100%" height="100%" src="https://invidious.snopyta.org/embed/Mml7rXLQoTQ" frameborder="0" allow="accelerometer;' +
+            modalbody = '<div class="youtubevod" style="height:75vh;"><iframe width="100%" height="100%" src="https://yewtu.be/embed/Mml7rXLQoTQ" frameborder="0" allow="accelerometer;' +
                 ' autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>';
             break;
         case 8://leftism today
             open = true;
             modaltitle = 'INFRARED VS LEFTISM TODAY';
-            modalbody = '<div class="youtubevod" style="height:75vh;"><iframe width="100%" height="100%" src="https://invidious.snopyta.org/embed/mDM2ojVYdrw" frameborder="0" allow="accelerometer;' +
+            modalbody = '<div class="youtubevod" style="height:75vh;"><iframe width="100%" height="100%" src="https://yewtu.be/embed/mDM2ojVYdrw" frameborder="0" allow="accelerometer;' +
                 ' autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>';            
             break;
         case 9://socialism done left
             open = true;
             modaltitle = 'INFRARED VS SOCIALISM DONE LEFT';
-            modalbody = '<div class="youtubevod" style="height:75vh;"><iframe width="100%" height="100%" src="https://invidious.snopyta.org/embed/xIlceNoYvGs" frameborder="0" allow="accelerometer;' +
+            modalbody = '<div class="youtubevod" style="height:75vh;"><iframe width="100%" height="100%" src="https://yewtu.be/embed/xIlceNoYvGs" frameborder="0" allow="accelerometer;' +
                 ' autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>';            
             break;
         case 10://xanderthal
             open = true;
             modaltitle = 'INFRARED VS XANDERTHAL';
-            modalbody = '<div class="youtubevod" style="height:75vh;"><iframe width="100%" height="100%" src="https://invidious.snopyta.org/embed/FFsIPOuOBg4" frameborder="0" allow="accelerometer;' +
+            modalbody = '<div class="youtubevod" style="height:75vh;"><iframe width="100%" height="100%" src="https://yewtu.be/embed/FFsIPOuOBg4" frameborder="0" allow="accelerometer;' +
                 ' autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>';            
             break;
         case 11://kevin logan
             open = true;
             modaltitle = 'INFRARED VS KEVIN LOGAN';
-            modalbody = '<div class="youtubevod" style="height:75vh;"><iframe width="100%" height="100%" src="https://invidious.snopyta.org/embed/CitM_kAZ_9M" frameborder="0" allow="accelerometer;' +
+            modalbody = '<div class="youtubevod" style="height:75vh;"><iframe width="100%" height="100%" src="https://yewtu.be/embed/CitM_kAZ_9M" frameborder="0" allow="accelerometer;' +
                 ' autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>';            
             break;
         case 12://dylan burns tv and sondsol
         case 13:
             open = true;
             modaltitle = 'INFRARED VS DYLAN BURNS TV & SONDSOL';
-            modalbody = '<div class="youtubevod" style="height:75vh;"><iframe width="100%" height="100%" src="https://invidious.snopyta.org/embed/7UpCH57lcbQ" frameborder="0" allow="accelerometer;' +
+            modalbody = '<div class="youtubevod" style="height:75vh;"><iframe width="100%" height="100%" src="https://yewtu.be/embed/7UpCH57lcbQ" frameborder="0" allow="accelerometer;' +
                 ' autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>';            
             break;
     }
