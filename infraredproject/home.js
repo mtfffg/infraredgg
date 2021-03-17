c49010adcea7d8e2bac46afec26945830603e685
mtfffg <mario.younis@yahoo.com>
2021-03-16 21:19:46 -0400

    Add files via upload

--- infraredproject/home.js
+++ infraredproject/home.js
@@ -1,30 +1,23 @@
-$(document).ready(function () {
-    //if (window.location.href.indexOf("www") < 0) {
-    //    window.location.href = 'https://www.infrared.gg';
-    //}
-});
-
-
 $('.mainGlobe').click(function () {
     if ($('.debateDiv').is(':visible')) {
         $('.debateDiv').css('display', 'none');
         $('#twitterDiv').css('width', '25%');
         $('.viddiv').css('left', '25%').css('width', '75%');
         $('.risingDiv').hide();
-        $('#fightsong')[0].pause()
-        $('#fightsong').hide();
+        showAudio('hide');
+        showDebateBoxes('hide');
     }
     else {
         $('.debateDiv').css('display', 'inline-block');
         $('#twitterDiv').css('width', '20%');
         $('.viddiv').css('left', '40%').css('width', '60%');
         $('.risingDiv').show();
-        $('#fightsong').show();
-        $('#fightsong')[0].play();
+        showAudio('show');
+        showDebateBoxes('show');
     }
 });
 
-$('.debatebox').click(function () {
+$(document).on('click', '.debatebox', function () {
     var id = $(this).data('id');    
     var open = false;
     var modalbody = '';
@@ -100,16 +93,106 @@ $('.modaldebateClose').click(function () {
     $('.debatevidBody').empty();
 });
 
-//$('.linkheader').mouseover(function () {
-//    $('.linkscollection').slideDown();
-//});
+function showDebateBoxes(type) {
+    if (type == 'show') {
+        if ($('.debateDiv .maincolorButton').length) {
+            $('.debateDiv').show();
+        }
+        else {
+            var debatebox = '<label class="maincolorButton mainLabel">DEBATE LIST</label>' +
+                ' <div class="debatebox" data-id="1" > ' +
+                ' <img class="debateimg" src = "infrareddebates/biden.jpg" /><br /><br />' +
+                ' <label class="debatename maincolorButton" > BIDEN</label> ' +
+                ' </div> ' +
+                ' <div class="debatebox" data-id="2"> ' +
+                ' <img class="debateimg" src = "infrareddebates/aoc.jpg" /><br /><br />' +
+                ' <label class="debatename maincolorButton" > AOC</label> ' +
+                ' </div > ' +
+                ' <div class="debatebox" data-id="3"> ' +
+                ' <img class="debateimg" src = "infrareddebates/thoughtslime.png" /><br /><br />' +
+                ' <label class="debatename maincolorButton" > THOUGHTSLIME</label> ' +
+                ' </div > ' +
+                ' <div class="debatebox" data-id="4" > ' +
+                ' <img class="debateimg" src = "infrareddebates/Vaush.jpg" /><br /><br />' +
+                ' <label class="debatename maincolorButton" > VAUSH</label> ' +
+                ' <img class="defeated" src = "infrareddebates/redX.png" /> ' +
+                ' </div> ' +
+                ' <div class="debatebox" data-id="5">' +
+                ' <img class="debateimg" src = "infrareddebates/destiny.jpg" /><br /><br />' +
+                ' <label class="debatename maincolorButton" > DESTINY</label> ' +
+                ' <img class="defeated" src = "infrareddebates/redX.png" /> ' +
+                ' </div> ' +
+                ' <div class="debatebox" data-id="6" > ' +
+                '     <img class="debateimg" src="infrareddebates/fellowtraveler.jpg" /><br /><br />   ' +
+                '     <label class="debatename maincolorButton">FELLOW TRAVELER</label>                ' +
+                '     <img class="defeated" src="infrareddebates/redX.png" />                          ' +
+                ' </div>                                                                               ' +
+                ' <div class="debatebox" data-id="7">                                                  ' +
+                '     <img class="debateimg" src="infrareddebates/jolah.jpg" /><br /><br />            ' +
+                '     <label class="debatename maincolorButton">JOLAH</label>                          ' +
+                '     <img class="defeated" src="infrareddebates/redX.png" />                          ' +
+                ' </div>                                                                               ' +
+                ' <div class="debatebox" data-id="8">                                                  ' +
+                '     <img class="debateimg" src="infrareddebates/leftismtoday.jpg" /><br /><br />     ' +
+                '     <label class="debatename maincolorButton">LEFTISM TODAY</label>                  ' +
+                '     <img class="defeated" src="infrareddebates/redX.png" />                          ' +
+                ' </div>                                                                               ' +
+                ' <div class="debatebox" data-id="9">                                                  ' +
+                '     <img class="debateimg" src="infrareddebates/socialismdoneleft.png" /><br /><br />' +
+                ' <label class="debatename maincolorButton" > SOCIALISM DONE LEFT</label> ' +
+                ' <img class="defeated" src="infrareddebates/redX.png" />' +
+                ' </div > ' +
+                ' <div class="debatebox" data-id="10"> ' +
+                '     <img class="debateimg" src="infrareddebates/xanderthal.jpg" /><br /><br />' +
+                '     <label class="debatename maincolorButton">XANDERTHAL</label>              ' +
+                '     <img class="defeated" src="infrareddebates/redX.png" />                   ' +
+                ' </div>                                                                        ' +
+                ' <div class="debatebox" data-id="11">                                          ' +
+                '     <img class="debateimg" src="infrareddebates/kevinlogan.jpg" /><br /><br />' +
+                '     <label class="debatename maincolorButton">KEVIN LOGAN</label>             ' +
+                '     <img class="defeated" src="infrareddebates/redX.png" />                   ' +
+                ' </div>                                                                        ' +
+                ' <div class="debatebox" data-id="12">                                          ' +
+                '     <img class="debateimg" src="infrareddebates/dylanburns.jpg" /><br /><br />' +
+                '     <label class="debatename maincolorButton">DYLAN BURNS TV</label>          ' +
+                '     <img class="defeated" src="infrareddebates/redX.png" />                   ' +
+                ' </div>                                                                        ' +
+                ' <div class="debatebox" data-id="13">                                          ' +
+                '     <img class="debateimg" src="infrareddebates/sondsol2.png" /><br /><br />  ' +
+                '     <label class="debatename maincolorButton">SONDSOL</label>                 ' +
+                '     <img class="defeated" src="infrareddebates/redX.png" />                   ' +
+                ' </div>                                                                        ' +
+                ' <div class="debatebox" data-id="14">                                          ' +
+                '     <img class="debateimg" src="infrareddebates/leftypol.jpg" /><br /><br />  ' +
+                '     <label class="debatename maincolorButton">LEFTYPOL</label>                ' +
+                '     <img class="defeated" src="infrareddebates/redX.png" />                   ' +
+                ' </div>';
+            $('.debateDiv').append(debatebox);
+            $('.debateDiv').show();
+        }    
+    }
+    else if (type == 'hide') {
+        $('.debateDiv').hide();
+    }   
+}
 
-//$('.linkheader').mouseleave(function () {
-//    $('.linkscollection').slideUp();
-//});
+function showAudio(type) {
+    var audio = '<audio id="fightsong" controls style="display:none;"><source src="infraredsong.mp3" type="audio/mpeg">Your browser does not support the audio element.</audio>' +
+        '<div class="youtubevod risingDiv"><img src="https://j.gifs.com/r8KJLk.gif" style="width:100%;" /></div>';     
 
-
-
-//RewriteEngine on
-//RewriteCond % { HTTP_HOST } ^ infrared\.gg$
-//RewriteRule ^ /?$ "https\:\/\/www\.infrared\.gg\/" [R=301,L]
+    if (type == 'show') {
+        if ($('#fightsong').length) {
+            $('#fightsong').show();
+            $('#fightsong')[0].play();
+        }
+        else {
+            $('.ytdiv').prepend(audio);
+            $('#fightsong').show();
+            $('#fightsong')[0].play();
+        }
+    }
+    else if (type == 'hide') {
+        $('#fightsong')[0].pause()
+        $('#fightsong').hide();
+    }    
+}
